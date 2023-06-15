b273bf4672e86f773a6c5512211bcbc19fbc89ba
Zarwine <ange_550@hotmail.com>
2023-06-15 15:09:29 +0200

    Added query parameter functionality to read from URL in server

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 let http = require('http')
 let fs = require('fs')
+let url = require('url')
 
 let server = http.createServer((request, response) => {
-    fs.readFile('index.html', (err,data) => {
-        if(err) throw err
+
+    let query= url.parse(request.url, true).query
+
+    let name = query.name === undefined ? 'anonyme' : query.name
+    
+    fs.readFile('index.html', 'utf-8', (err,data) => {
+        if(err) {
+            response.writeHead(404)
+            response.end("404 Page non trouvée")
+        }
 
         response.writeHead(200, {
             'Content-type': 'text/html; charset=utf-8'
         })
+
+        data = data.replace('{{name}}', name)
         response.end(data)
     })
+    
 }).listen(8080)
