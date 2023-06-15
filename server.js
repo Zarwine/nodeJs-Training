faab81c429f8c2999d040377084d3ca79557ec5f
Zarwine <ange_550@hotmail.com>
2023-06-15 15:25:26 +0200

    Refactored code to use EventEmitter for handling root route

--- server.js
+++ server.js
@@ -1,7 +1,33 @@
 let http = require('http')
 let fs = require('fs')
 let url = require('url')
+const EventEmitter = require('events')
 
+let App = {
+    start: function(port) {
+        let emitter = new EventEmitter()
+        let server = http.createServer((request, response) => {
+            response.writeHead(200, {
+                'Content-type': 'text/html; charset=utf-8'
+            })
+            if(request.url === '/') {
+                emitter.emit('root', response)
+            }
+            response.end()
+
+        }).listen(port)
+
+        return emitter
+    }
+}
+
+let app = App.start(8080)
+
+app.on('root', function (response) {
+    response.write('test emitter')
+})
+
+/*
 let server = http.createServer((request, response) => {
 
     let query= url.parse(request.url, true).query
@@ -23,3 +49,4 @@ let server = http.createServer((request, response) => {
     })
     
 }).listen(8080)
+*/
